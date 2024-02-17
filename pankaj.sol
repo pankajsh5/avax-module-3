dac8d133b6016e133e147aba276e046c60f53d27
pankajsh5 <120113677+pankajsh5@users.noreply.github.com>
2024-02-17 22:38:26 +0530

    Update pankaj.sol

--- pankaj.sol
+++ pankaj.sol
@@ -1,23 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
+
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract PankajToken is ERC20, ERC20Burnable, Ownable {
-
     constructor(address initialOwner)
         ERC20("PankajToken", "PANK")
         Ownable(initialOwner)
     {
-        _mint(msg.sender, 1 * 10 ** decimals());
+        _mint(initialOwner, 1 * 10 ** decimals());
     }
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
 
-    function burn(uint256 amount) public override {
-        _burn(msg.sender, amount);
+    function burn(address from, uint256 amount) public {
+        _burn(from, amount);
+    }
+
+    function transfer(address to, uint256 amount) public override returns (bool) {
+        _transfer(msg.sender, to, amount);
+        return true;
     }
 }
